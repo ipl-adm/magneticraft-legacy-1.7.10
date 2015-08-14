3ee5cdaaa38771c30e7b2e15a9b42ed9bb24db0f
cout970 <thecout970@gmail.com>
2015-08-14 03:21:05 +0200

    fixed invalid comments on the api package-info files
    Signed-off-by: cout970 <thecout970@gmail.com>

--- src/main/java/com/cout970/magneticraft/api/computer/package-info.java
+++ src/main/java/com/cout970/magneticraft/api/computer/package-info.java
@@ -1,7 +1,4 @@
-/**
- * (C) 2014 Team CoFH / CoFH / Cult of the Full Hub
- * http://www.teamcofh.com
- */
+
 @API(apiVersion = com.cout970.magneticraft.api.MgAPI.VERSION, owner = "Magneticraft", provides = "MagneticraftAPI|Computer")
 package com.cout970.magneticraft.api.computer;
 
--- src/main/java/com/cout970/magneticraft/api/tool/package-info.java
+++ src/main/java/com/cout970/magneticraft/api/tool/package-info.java
@@ -1,7 +1,5 @@
-/**
- * (C) 2014 Team CoFH / CoFH / Cult of the Full Hub
- * http://www.teamcofh.com
- */
+
+
 @API(apiVersion = com.cout970.magneticraft.api.MgAPI.VERSION, owner = "Magneticraft", provides = "MagneticraftAPI|Tools")
 package com.cout970.magneticraft.api.tool;
 

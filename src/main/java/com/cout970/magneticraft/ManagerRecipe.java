158c4abfc4aa941141829a867de54907ef82d269
CypherAJ <ds98s3a@gmail.com>
2015-09-14 01:39:44 -0700

    Fixed a fix for smelting

--- src/main/java/com/cout970/magneticraft/ManagerRecipe.java
+++ src/main/java/com/cout970/magneticraft/ManagerRecipe.java
@@ -43,8 +43,9 @@ public class ManagerRecipe {
 //				GameRegistry.addSmelting(new ItemStack(rubble_clean.get(i)), ingot, 1.0F);
                 GameRegistry.addSmelting(new ItemStack(pebbles, 1, i), ingot, 1.0F);
 //				GameRegistry.addSmelting(new ItemStack(pebbles_clean.get(i)), ingot, 1.0F);
-                ingot.stackSize = 1;
-                GameRegistry.addSmelting(new ItemStack(dust, 1, i), ingot, 1.0F);
+                ItemStack ing2 = ingot.copy();
+                ing2.stackSize = 1;
+                GameRegistry.addSmelting(new ItemStack(dust, 1, i), ing2, 1.0F);
             }
         }
         GameRegistry.addSmelting(new ItemStack(oreCopper), new ItemStack(ingotCopper), 1.0F);
